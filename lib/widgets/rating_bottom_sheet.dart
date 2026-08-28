3cf670043704d5d23b4e253f3f87771e7492925c
Ryan Ernest <ryan@rivergroup.com>
2026-08-28 03:33:21 -0400

    refactor(media): centralize backend product names

--- lib/widgets/rating_bottom_sheet.dart
+++ lib/widgets/rating_bottom_sheet.dart
@@ -538,7 +538,7 @@ class _RatingBottomSheetState extends State<RatingBottomSheet> {
     return () => nodes[index].requestFocus();
   }
 
-  String _backendLabel(MediaBackend backend) => backend.dialect?.productName ?? 'Plex';
+  String _backendLabel(MediaBackend backend) => backend.productName;
 }
 
 const _serverKey = 'server';
