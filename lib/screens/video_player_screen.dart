d502a93a5b8b47f2e9404063675d9e2e20007d0d
Ryan Ernest <ryan@rivergroup.com>
2026-08-31 15:28:44 -0400

    feat(clips): add local video clip export for desktop

--- lib/screens/video_player_screen.dart
+++ lib/screens/video_player_screen.dart
@@ -56,6 +56,8 @@ import '../services/playback_coordinator.dart';
 import '../services/music/music_playback_service.dart';
 import '../services/playback_initialization_service.dart';
 import '../services/playback_context.dart';
+import '../services/clip_export_service.dart';
+import '../services/clip_preview_player_controller.dart';
 import '../services/local_playback_history.dart';
 import '../services/playback_session.dart';
 import '../services/playback_subtitle_resolver.dart';
@@ -117,6 +119,7 @@ import 'video_player/visual_effects_controller.dart';
 import 'video_player/widgets/player_prompt_overlays.dart';
 import '../widgets/overlay_sheet.dart';
 import '../widgets/video_controls/player_chrome_controller.dart';
+import '../widgets/video_controls/sheets/clip_editor_sheet.dart';
 import '../widgets/video_controls/video_controls.dart';
 import '../widgets/video_controls/widgets/player_toast_indicator.dart';
 import '../focus/focusable_button.dart';
@@ -130,6 +133,7 @@ import '../watch_together/providers/watch_together_provider.dart';
 import '../watch_together/services/watch_together_controller.dart';
 
 part 'video_player/parts/companion_remote.dart';
+part 'video_player/parts/clips.dart';
 part 'video_player/parts/display_matching.dart';
 part 'video_player/parts/episode_navigation.dart';
 part 'video_player/parts/episode_queue.dart';
@@ -1212,6 +1216,7 @@ class VideoPlayerScreenState extends State<VideoPlayerScreen> with WidgetsBindin
   MediaControlRouter debugMediaControlRouterForTesting() => _buildMediaControlRouter();
 
   late final PlayerNavigationCoordinator _playerNavigationCoordinator;
+  late final ClipExportService _clipExportService = ClipExportService();
 
   @override
   void initState() {
@@ -1956,7 +1961,8 @@ class VideoPlayerScreenState extends State<VideoPlayerScreen> with WidgetsBindin
       }
 
       if (PlatformDetector.isDesktopOS()) {
-        await currentPlayer.setProperty('screenshot-directory', '~/Pictures');
+        final screenshotDirectory = await ClipExportService.screenshotDirectory();
+        await currentPlayer.setProperty('screenshot-directory', screenshotDirectory.path);
       }
 
       final customMpvConfig = SettingsService.parseMpvConfigText(settingsService.read(SettingsService.mpvConfigText));
@@ -2311,6 +2317,7 @@ class VideoPlayerScreenState extends State<VideoPlayerScreen> with WidgetsBindin
     _isExiting.dispose();
     _chromeController.dispose();
     _toastController.dispose();
+    _clipExportService.dispose();
 
     // The release sequence below mirrors _tearDownFailedPlayerAttempt but is
     // deliberately separate: dispose() cannot await, and it destroys the
