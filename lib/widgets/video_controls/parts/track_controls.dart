bc021084bff33888b9edaa9dd45fb6d31c50a285
Ryan Ernest <ryan@rivergroup.com>
2026-08-31 15:28:44 -0400

    feat(clips): add local video clip export for desktop

--- lib/widgets/video_controls/parts/track_controls.dart
+++ lib/widgets/video_controls/parts/track_controls.dart
@@ -210,6 +210,14 @@ extension _PlexVideoControlsTrackMethods on _PlexVideoControlsState {
       onSubtitleTrackChanged: _onSubtitleTrackChanged,
       onSecondarySubtitleTrackChanged: widget.onSecondarySubtitleTrackChanged,
       onRateRequested: widget.onRateRequested,
+      onClipRequested:
+          shouldShowClipButton(
+            isLive: widget.isLive,
+            isDesktop: PlatformDetector.isDesktopOS(),
+            hasClipHandler: widget.onClipRequested != null,
+          )
+          ? widget.onClipRequested
+          : null,
       onCancelAutoHide: widget.chromeController.cancelAutoHide,
       onStartAutoHide: _startHideTimer,
       serverId: widget.metadata.serverId,
