4c90b50c30dfb350c98f9c7c0d050e3ad6ac9f6c
Ryan Ernest <ryan@rivergroup.com>
2026-08-31 15:28:44 -0400

    feat(clips): add local video clip export for desktop

--- lib/screens/video_player/parts/build.dart
+++ lib/screens/video_player/parts/build.dart
@@ -313,6 +313,7 @@ extension _VideoPlayerBuildMethods on VideoPlayerScreenState {
                         onAudioTrackChanged: _onAudioTrackChanged,
                         onSubtitleTrackChanged: _onSubtitleTrackChanged,
                         onSecondarySubtitleTrackChanged: _onSecondarySubtitleTrackChanged,
+                        onClipRequested: _canClipCurrentPlayback ? () => _handleClipRequested(context) : null,
                         onSeekRequested: _seekPlayback,
                         onRateRequested: _setPlaybackRate,
                         onPlayPauseRequested: _handleControlsTransport,
